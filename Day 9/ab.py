87c1694a882903c03aa56446e5ef5f4038b01276
Jeffrey <trainmyselfonline@gmail.com>
2023-04-04 13:23:33 +0800

    adding day 9 files

---
+++ Day 9/ab.py
@@ -0,0 +1,23 @@
+# # Approach1
+# import a
+# import b
+#
+# obj1 = a.Animal()
+# obj1.display()
+#
+# obj2 = b.Bird()
+# obj2.display()
+
+# Approach2
+
+from a import Animal
+from b import Bird
+
+obj1 = Animal()
+obj2 = Bird()
+
+obj1.display()
+obj2.display()
+
+Animal().display()
+Bird().display()
---
+++ Day 9/client.py
@@ -0,0 +1,19 @@
+
+import sys
+sys.path.append("C:/Training/SeleniumPython/Day 9/pack1")
+#
+# # Approach1
+# import module1
+# import module2
+#
+# module1.display()
+# module2.show()
+
+# # Approach 2
+# from module1 import *
+# from module2 import *
+#
+#
+# display()
+# show()
+
