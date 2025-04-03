c732e733026c6a9ae8adf16f0b199ccd2f0995c7
Jeffrey <trainmyselfonline@gmail.com>
2025-04-03 10:43:28 +0800

    completed login.feature

--- BehaveBDDFramework/features/login.feature
+++ BehaveBDDFramework/features/login.feature
@@ -4,27 +4,33 @@ Feature: Login Functionality
   Scenario: Login with valid credentials
     Given I navigated to Login page
     When I enter valid email address and valid password into the fields
+     And I click on Login button
     Then I should get logged in
 
+  @completed
   Scenario: Login with invalid email and valid password
     Given I navigated to Login page
     When I enter invalid email and valid password into the field
     And I click on Login button
     Then I should get a proper warning message
 
+  @completed
   Scenario: Login with valid email and invalid password
     Given I navigated to Login page
     When I enter valid email and invalid password into the field
     And I click on Login button
     Then I should get a proper warning message
 
+  @completed
   Scenario: Login with invalid credentials
     Given I navigated to Login page
     When I enter invalid email and invalid password into the field
     And I click on Login button
     Then I should get a proper warning message
 
+  @completed
   Scenario: Login without entering any credentials
     Given I navigated to Login page
     When I dont enter anything into email and password fields
+    And I click on Login button
     Then I should get a proper warning message
--- BehaveBDDFramework/features/steps/login.py
+++ BehaveBDDFramework/features/steps/login.py
@@ -12,10 +12,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @given(u'I navigated to Login page')
 def step_impl(context):
-    # option = Options()
-    # option.add_experimental_option("detach", True)
-    # context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-    context.driver = webdriver.Chrome()
+    option = Options()
+    option.add_experimental_option("detach", True)
+    context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
     context.driver.maximize_window()
     context.driver.get("https://tutorialsninja.com/demo/index.php?route=account/login")
     context.driver.maximize_window()
@@ -32,11 +31,13 @@ def step_impl(context):
     actual_text = WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "content"))).text
     print(actual_text)
     assert expected_text in actual_text
+    context.driver.quit()
 
 
 @when(u'I enter invalid email and valid password into the field')
 def step_impl(context):
-    print("Inside Step")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-email"))).send_keys("CarlMax@email_invalid.com")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-password"))).send_keys("12345")
 
 
 @when(u'I click on Login button')
@@ -47,19 +48,26 @@ def step_impl(context):
 
 @then(u'I should get a proper warning message')
 def step_impl(context):
-    print("Inside Step")
+    expected_message = "Warning: No match for E-Mail Address and/or Password."
+    actual_message = WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.alert'))).text
+    print(f"Actual: {actual_message}")
+    assert expected_message.__eq__(actual_message)
+    context.driver.quit()
 
 
 @when(u'I enter valid email and invalid password into the field')
 def step_impl(context):
-    print("Inside Step")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-email"))).send_keys("CarlMax@email.com")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-password"))).send_keys("12345_invalid")
 
 
 @when(u'I enter invalid email and invalid password into the field')
 def step_impl(context):
-    print("Inside Step")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-email"))).send_keys("CarlMax@email_invalid.com")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-password"))).send_keys("12345_invalid")
 
 
 @when(u'I dont enter anything into email and password fields')
 def step_impl(context):
-    print("Inside Step")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-email"))).send_keys("")
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.ID, "input-password"))).send_keys("")
